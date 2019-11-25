ad60b67ff155a0544f6a70c4a7eb034b4f118055
Adrienne Yao <adrienne.yao@icloud.com>
2019-11-25 14:48:54 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -97,8 +97,6 @@ def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   movie= Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
   movie.delete
-end
-
 def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
